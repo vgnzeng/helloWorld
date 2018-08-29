0262b00449c6afedba62bb3c38d44f42a5c32e92
Guannian Zeng <vgnzeng@gmail.com>
2018-08-28 22:18:43 -0700

    finished hot swap for IDEA

--- src/main/java/com/zeng/helloWorld/web/HelloWorldController.java
+++ src/main/java/com/zeng/helloWorld/web/HelloWorldController.java
@@ -8,9 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 
     @RequestMapping("/hello")
-    public String hello() {
-        return "Hello World";
+    public String index(String name) {
+        return "Hello World, " +name;
     }
-
 }
-
